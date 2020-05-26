56577b3757103a8b0943d535dbac56373893d8d4
Aaron Warsylewicz <amw124@gmail.com>
2020-05-25 23:56:52 -0600

    Added some router paths

--- stucky-notes-web/src/App.js
+++ stucky-notes-web/src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./App.css";
-import Auth from './auth/Auth';
+import Auth from "./auth/Auth";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Auth />
-    </div>
+    <Router>
+      <div className="App">
+      <Switch>
+        <Route path="/home">
+          <p>hi</p>
+        </Route>
+        <Route path="/">
+          <Auth />
+        </Route>
+      </Switch>
+      
+        
+      </div>
+    </Router>
   );
 }
 
